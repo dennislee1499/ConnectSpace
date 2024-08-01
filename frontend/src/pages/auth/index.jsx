4bb70d3beaafc1f3cc54ff6a836dda42fe62fc16
Dennis Lee <dennislee1499@gmail.com>
2024-08-01 15:12:02 -0400

    Styled auth page - Login/Signup

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,7 +1,96 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import backgroundImage from "../../assets/background.jpg";
+
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState(""); 
+
   return (
-    <div>Auth</div>
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center"
+    style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: "center"
+    }}
+    >
+        <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl
+        w-[80vw] md:w-[70vw] lg:w-[60vw] xl:w-[50vw] rounded-3xl flex items-center justify-center">
+            <div className="flex flex-col gap-10 items-center justify-center">
+                <div className="flex items-center justify-center flex-col">
+                    <div className="flex items-center justify-center mb-5">
+                        <h1 className="text-4xl font-bold md:text-5xl">ConnectSpace</h1>
+                    </div>
+                    <p className="font-medium text-center">Fill in details to start connecting!</p>
+                </div>
+                <div className="flex items-center justify-center w-full">
+                    <Tabs className="w-3/4">
+                        <TabsList className="bg-transparent rounded-none w-full">
+                            <TabsTrigger value="login"
+                            className="data-[state=active]:bg-transparent text-opacity-90 
+                            border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold
+                            data-[state=active]:border-b-blue-500 p-3 transition-all duration-300"
+                            >
+                                Login
+                            </TabsTrigger>
+                            <TabsTrigger value="signup"
+                            className="data-[state=active]:bg-transparent text-opacity-90 
+                            border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold
+                            data-[state=active]:border-b-blue-500 p-3 transition-all duration-300"
+                            >
+                                Signup
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+                            <Input 
+                              placeholder="Email" 
+                              type="email" 
+                              className="rounded-full p-6" 
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}  
+                            />
+                            <Input 
+                              placeholder="Password" 
+                              type="password" 
+                              className="rounded-full p-6" 
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}  
+                            />
+                            <Button className="rounded-full p-6">Login</Button>
+                        </TabsContent>
+                        <TabsContent className="flex flex-col gap-5" value="signup">
+                            <Input 
+                              placeholder="Email" 
+                              type="email" 
+                              className="rounded-full p-6" 
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}  
+                            />
+                            <Input 
+                              placeholder="Password" 
+                              type="password" 
+                              className="rounded-full p-6" 
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}  
+                            />
+                            <Input 
+                              placeholder="confirm Password" 
+                              type="password" 
+                              className="rounded-full p-6" 
+                              value={confirmPassword}
+                              onChange={(e) => setConfirmPassword(e.target.value)}  
+                            />
+                            <Button className="rounded-full p-6">Sign Up</Button>
+                        </TabsContent>
+                    </Tabs>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
 export default Auth;
+
