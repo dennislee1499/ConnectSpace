befb5458f56252202d9a25ae1f327376c0b1e874
Dennis Lee <dennislee1499@gmail.com>
2024-08-11 15:01:40 -0400

    Setup route guards, state check in Chat component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,48 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './pages/auth';
 import Chat from './pages/chat';
 import Profile from './pages/profile';
+import { useAppStore } from './store';
+
+const UnauthorizedRoute = ({ children }) => {
+  const { userInfo } = useAppStore(); 
+  const isAuthorized = !!userInfo; 
+  return isAuthorized ? children : <Navigate to="/auth" />
+}
+
+const AuthorizedRoute = ({ children }) => {
+  const { userInfo } = useAppStore(); 
+  const isAuthorized = !!userInfo; 
+  return isAuthorized ? <Navigate to="/chat" /> : children; 
+}
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={ <Auth /> } />
-        <Route path="/profile" element={ <Profile /> } />
-        <Route path="/chat" element={ <Chat /> } />
+        <Route 
+          path="/auth" 
+          element={ 
+            <AuthorizedRoute>
+              <Auth />
+            </AuthorizedRoute> 
+          } 
+        />
+        <Route 
+          path="/profile" 
+          element={ 
+            <UnauthorizedRoute>
+              <Profile />
+            </UnauthorizedRoute> 
+          } 
+        />
+        <Route 
+          path="/chat" 
+          element={ 
+            <UnauthorizedRoute>
+              <Chat />
+            </UnauthorizedRoute> 
+          } 
+        />
         <Route path="*" element={ <Navigate to="/auth" /> } />
       </Routes>
     </BrowserRouter>
